166cf73d5d5bd24498f7878cf3303e57969a5806
Dax2976 <98029197+Dax2976@users.noreply.github.com>
2022-06-22 14:04:05 +0200

    add show and view fix faker

--- laravel-boolpress/app/Http/Controllers/Admin/PostController.php
+++ laravel-boolpress/app/Http/Controllers/Admin/PostController.php
@@ -4,9 +4,17 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Model\Post;
 
 
 class PostController extends Controller
 {
-    //
+    public function index(){
+        $posts = Post::all();
+        return view('admin.posts.index', compact('posts'));
+    }
+
+    public function show(Post $post){
+        return view('admin.posts.show',compact('post'));
+    }
 }
---
+++ laravel-boolpress/resources/views/admin/posts/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        <div class="row">
+            @foreach ($posts as $post)
+            <div class="col border m-1">
+                <h3>{{$post->title}}</h3>
+                <img src="{{$post->image}}" alt="">
+                <p>{{$post->description}}</p>
+                <div>
+                    <a href="{{route('admin.posts.show',$post->id)}}">View</a>
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </div>
+@endsection 
